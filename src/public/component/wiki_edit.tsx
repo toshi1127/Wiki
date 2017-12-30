ad9427788f70bef07a2320881199bcc6459df3d8
USER <user@kyokuyamakenichinoMacBook-Pro.local>
2017-12-31 00:10:15 +0900

    Add:interface

--- src/public/component/wiki_edit.tsx
+++ src/public/component/wiki_edit.tsx
@@ -3,16 +3,32 @@ import * as request from 'superagent'
 import { Redirect } from 'react-router-dom'
 
 interface IndexProps {
-
+    name:string
 }
 interface IndexState {
-
+    name:any,
+    body:string,
+    loaded:boolean,
+    jump:string
 }
 
 export default class WikiEdit extends React.Component<IndexProps, IndexState>  {
-    constructor(props:any) {
+    constructor(props: any) {
         super(props);
+        //URLのパラメーターを受け取っている。
+        const name = this.props.name
         this.state = {
+            name, 
+            body: '',
+            loaded: false,
+            jump: ''
         }
+    }
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
     }
 }
