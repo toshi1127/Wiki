5a4d69c18e1b511e33d9ab5d328e2813201fb165
USER <user@kyokuyamakenichinoMacBook-Pro.local>
2017-12-30 19:32:43 +0900

    Fix:tsconfig setting

---
+++ src/public/component/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import {
+    BrowserRouter,
+    Route, Switch
+} from 'react-router-dom'
+const WikiApp = () => (
+    <BrowserRouter>
+    </BrowserRouter>
+)
+
+ReactDOM.render(
+    <WikiApp />,document.getElementById('root')
+)
