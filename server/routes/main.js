2261e7d4c65d0b3fffb394baf4d2072279a18bc5
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-21 02:34:15 +0900

    不要なファイルの削除

--- server/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-var router = express.Router();
-router.get('/',(req,res)=>{
-    if (!req.user) {
-        res.redirect('/auth/login');
-    }
-    res.render('index');
-});
-module.exports = router;
