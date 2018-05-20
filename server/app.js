c37e77a148984b6ccbd4e87515f5ea350c2c82d1
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-20 16:40:37 +0900

    make better dir

--- server/app.js
+++ server/app.js
@@ -1,24 +1,24 @@
 "use strict";
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const Nedb = require("nedb");
+const passport = require('passport');
+const session = require('express-session');
+const auth = require('./routes/auth');
+const main = require('./routes/main');
 
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var Nedb = require("nedb");
-var passport = require('passport');
-var session = require('express-session');
-var auth = require('./routes/auth');
-var main = require('./routes/main');
-
-var db = new Nedb({
+const db = new Nedb({
     filename: path.join(__dirname, 'wiki.db'),
     autoload: true
 });
-var app = express();
-app.get('/', function (req, res) {
-    console.log("aiueo");
+let app = express();
+app.get('/', (req, res) => {
+    console.log("aiueo")
     if (!req.user) {
         res.redirect('/auth/login');
-    } else {
+    }
+    else {
         res.redirect(302, '/main');
     }
 });
@@ -27,8 +27,8 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 30
-    }
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+    },
 }));
 passport.serializeUser(function (user, done) {
     done(null, user);
@@ -45,8 +45,8 @@ app.use('/auth', auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 var port = '8080';
-app.listen(process.env.PORT || port, function () {
-    console.log('起動しました', "http://localhost:" + port);
+app.listen(process.env.PORT || port, () => {
+    console.log('起動しました', `http://localhost:${port}`);
     console.log('Server listening at https://' + ':' + process.env.PORT);
     console.log(process.env.IP);
 });
@@ -56,10 +56,10 @@ app.use('/edit/:wikiname', express.static(__dirname + '/public'));
 app.use('/main/:name', express.static(__dirname + '/public'));
 // APIの定義
 // Wikiデータを返すAPI 
-app.get('/api/get/:wikiname', function (req, res) {
-    var wikiname = req.params.wikiname;
+app.get('/api/get/:wikiname', (req, res) => {
+    const wikiname = req.params.wikiname;
     //この時点でundifindなので、componentWillMountの時点でstate.nameに値が入っていない。
-    db.find({ name: wikiname }, function (err, docs) {
+    db.find({ name: wikiname }, (err, docs) => {
         if (err) {
             res.json({ status: false, msg: err });
             return;
@@ -70,50 +70,53 @@ app.get('/api/get/:wikiname', function (req, res) {
         res.json({ status: true, data: docs[0] });
     });
 });
-app.get('/api/getting_list', function (req, res) {
-    db.find({}, function (err, docs) {
+app.get('/api/getting_list', (req, res) => {
+    db.find({}, (err, docs) => {
         if (err) {
             res.json({ status: false, msg: err });
             return;
-        } else {
+        }
+        else {
             //ここで名前の配列を作ってクライアントに返す。
-            docs.map(function (value, index, array) {
+            docs.map((value, index, array) => {
                 array[index] = value.name;
             });
             res.json({ status: true, data: docs });
         }
     });
 });
-app.get('/create/:wikiname/:name', function (req, res) {
-    var wikiname = req.params.wikiname;
-    var name = req.params["name"];
-    db.insert([{
-        name: wikiname,
-        user: name
-    }], function (err, newDoc) {
+app.get('/create/:wikiname/:name', (req, res) => {
+    const wikiname = req.params.wikiname;
+    const name = req.params["name"]
+    db.insert([
+        {
+            name: wikiname,
+            user: name
+        }
+    ], function (err, newDoc) {
         console.log(newDoc);
     });
 });
-app.get('/delete/:wikiname', function (req, res) {
-    var wikiname = req.params.wikiname;
+app.get('/delete/:wikiname', (req, res) => {
+    const wikiname = req.params.wikiname;
     db.remove({ name: wikiname }, function (err, deleteDoc) {
         console.log(deleteDoc);
     });
 });
-app.post('/api/put/:wikiname/:name', function (req, res) {
-    var wikiname = req.params.wikiname;
-    var user = req.params["name"];
+app.post('/api/put/:wikiname/:name', (req, res) => {
+    const wikiname = req.params.wikiname;
+    const user = req.params["name"]
     // 既存のエントリがあるか確認
-    db.find({ 'name': wikiname, 'user': user }, function (err, docs) {
+    db.find({ 'name': wikiname, 'user': user }, (err, docs) => {
         if (err) {
             res.json({ status: false, msg: err });
             return;
         }
-        var body = req.body.body;
-        console.log(body);
-        console.log(wikiname);
-        console.log(user);
-        db.update({ name: wikiname, user: user }, { name: wikiname, user: user, body: body });
+        const body = req.body.body;
+        console.log(body)
+        console.log(wikiname)
+        console.log(user)
+        db.update({ name: wikiname, user: user }, { name: wikiname, user: user, body });
 
         res.json({ status: true });
     });
