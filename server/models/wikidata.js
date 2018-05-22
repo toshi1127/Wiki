84c4bba513781245c7325dc59c414895f80b5733
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-23 02:04:59 +0900

    モデルの定義

--- server/models/wikidata.js
+++ server/models/wikidata.js
@@ -1,10 +1,17 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
+var elementSchema = mongoose.Schema({
     _id: String,
     name: String,
     body: String,
     user: String
+})
+
+const wikiListSchema = mongoose.Schema({
+    _id: String,
+    work: [elementSchema],
+    evaluation: [elementSchema],
+    result: [elementSchema]
 });
 
-export default mongoose.model('WikiDate', userSchema);
+//export default mongoose.model('WikiList', wikiListSchema);
