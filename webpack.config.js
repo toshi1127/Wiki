5a9014bc9c6e4506ed104f99b84bdd1f8d43b8d7
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-25 23:33:56 +0900

    typescriptをコンパイルするように変するように変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  entry: path.join(__dirname, 'dist/public/component/index.js'),
+  entry: path.join(__dirname, 'src/public/component/index.tsx'),
   output: {
     path: path.join(__dirname, 'dist/public'),
     filename: 'bundle.js'
