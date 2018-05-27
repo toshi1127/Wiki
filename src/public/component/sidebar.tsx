7090091c8fe2d78192aa882fc0ac22eada364b7e
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-28 02:15:53 +0900

    mainコンポーネントにイベントを送るように週送るように修正

--- src/public/component/sidebar.tsx
+++ src/public/component/sidebar.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 import { media } from '../utile/Helper'
 
 interface IndexProps {
-
+    onClick: any
 }
 interface IndexState {
 
@@ -21,7 +21,8 @@ const SideBarComponent = styled('section') `
 `;
 
 const Liststyle = {
-    color: 'white'
+    color: 'white',
+    width: '95%'
 };
 
 const style = {
@@ -32,18 +33,26 @@ export default class SideBar extends React.Component<IndexProps, IndexState> {
     constructor(props: IndexProps) {
         super(props)
     }
+    onClick(e: any, v: any) {
+        if (this.props.onClick) {
+            this.props.onClick({
+                value: v
+            })
+        }
+    }
     render() {
+        const onClick = (e: any, v: any) => this.onClick(e, v)
         return (
             <MuiThemeProvider>
                 <SideBarComponent>
                     <Paper zDepth={2} style={style} >
-                        <Menu>
-                            <MenuItem primaryText="練習記録" style={Liststyle} />
-                            <MenuItem primaryText="反省会議事録" style={Liststyle} />
-                            <MenuItem primaryText="過去の戦歴" style={Liststyle} />
-                            <MenuItem primaryText="運行記録簿" style={Liststyle} />
-                            <MenuItem primaryText="車両点検表" style={Liststyle} />
-                            <MenuItem primaryText="部内規則" style={Liststyle} />
+                        <Menu onChange={onClick}>
+                            <MenuItem value="練習記録" primaryText="練習記録" style={Liststyle} />
+                            <MenuItem value="反省会議事録" primaryText="反省会議事録" style={Liststyle} />
+                            <MenuItem value="過去の戦歴" primaryText="過去の戦歴" style={Liststyle} />
+                            <MenuItem value="運行記録簿" primaryText="運行記録簿" style={Liststyle} />
+                            <MenuItem value="車両点検表" primaryText="車両点検表" style={Liststyle} />
+                            <MenuItem value="部内規則" primaryText="部内規則" style={Liststyle} />
                         </Menu>
                     </Paper>
                 </SideBarComponent>
